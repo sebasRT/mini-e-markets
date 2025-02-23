3303a3369c08c6a69f1dd04a7b30502a916d94ab
sebasRT <rtsebas11@gmail.com>
2025-02-22 22:25:30 -0500

    Add Redis integration for push token management

---
+++ src/app/api/set-push-token/route.ts
@@ -0,0 +1,12 @@
+import { setPushToken } from "@/lib/redis";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    const token = await request.text()
+
+    if (!token) {
+        return NextResponse.json("Token not received", { status: 400 })
+    }
+
+    return await setPushToken(token).then(token => NextResponse.json(token)).catch(error => NextResponse.json(error.message, { status: 500 }))
+}
---
+++ src/lib/redis/index.ts
@@ -0,0 +1,15 @@
+import { Redis } from '@upstash/redis'
+
+const redis = new Redis({
+  url: process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
+})
+
+export async function setPushToken(token: string) {
+    
+    try {
+        return await redis.set('push-token', token);
+    } catch (error: any) {
+        throw error.message
+    }
+}
